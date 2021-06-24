e719612243c3b7a32d63e33ea6327da8c24589a2
andrea.guido <andrea.guido.research@gmail.com>
2021-06-24 18:02:52 +0200

    fixed DG norm and choice data. now we can extrapolate mean and variance

--- 2016Kim003.R
+++ 2016Kim003.R
@@ -3,12 +3,13 @@ library(Hmisc)
 library(foreign)
 library(tidyverse)
 
+rm(list = ls())
 # set wd
 setwd("G:/.shortcut-targets-by-id/1IoJDOQWCFiL1qTzSja6byrAlCelNSTsT/Meta-analysis beliefs/Dati paper/2016Kim003/2016Kim003_data/Data_JEEA_MS_5107")
 
-pgs=read.csv("/PG_Data_bysubj.txt", sep="\t")
-tgs=read.csv("/TG_Data_bysubj.txt", sep="\t")
-ug=read.csv("/UG_Data.txt", sep="\t")
+pgs=read.csv("PG_Data_bysubj.txt", sep="\t")
+tgs=read.csv("TG_Data_bysubj.csv", sep="\t")
+ug=read.csv("UG_Data.txt", sep="\t")
 dg=read.csv("DG_Data.csv", sep="\t")
 ## the next file contains all data except the conditional PG elicitations, which we did later
 norms1=read.csv("Norm_Elicitation_Data.csv")
@@ -22,19 +23,35 @@ norms1=read.csv("Norm_Elicitation_Data.csv")
 ## role : 1 proposer; 0 responder
 ## sent : amount sent; -1 if not applicable
 coldg = c("exp_id","exp_num","subj_id","subj", "role", "sent")
-dg_dta_coop <- dg %>% subset.data.frame(select = coldg, subset = role == 1) %>% mutate(endowment = 16, cooperation = sent/endowment) %>% 
-  summarise(mean_cooperation = mean(cooperation))
 
-dg_dta_var <- dg %>% subset.data.frame(select = coldg, subset = role == 1) %>% mutate(endowment = 16, cooperation = sent/endowment) %>% 
-  summarise(mean_cooperation = var(cooperation))
+## compute average donation
+dg_dta_coop <- dg %>% subset.data.frame(select = coldg, subset = role == 1) %>% 
+  mutate(endowment = 16, cooperation = sent/endowment) %>% 
+  summarise(mean_cooperation = mean(cooperation))
+## compute variance donations
+dg_dta_var <- dg %>% subset.data.frame(select = coldg, subset = role == 1) %>% 
+  mutate(endowment = 16, cooperation = sent/endowment) %>% 
+  summarise(var_cooperation = var(cooperation))
+
 ## answers[1-9] : kw appropriateness
 ## actions: send 16/0; 14/2; 12/4; ...; 0/16
 ## exp_id
 ## session
 ## subject
 ## KW scale: 1: VI; 2: I; 3: A; 4: VA
-dg_norms <- norms1[, c(1, 3, 4, 11:19)] %>% summarise(sum(norms1[,11]))
-label_col = paste("answer.",1,".", sep = "")
+label_col = as.character(seq(0,16,2))
+dg_columns <- c(1, 3, 4, 11:19)
+## compute norm 
+dg_appropriateness_sum <- norms1[, dg_columns] %>% summarise_at(vars(answers.1.:answers.9.), sum, na.rm=T) 
+dg_norm <- as.integer(label_col[which.max(dg_appropriateness_sum)])
+
+## compute variance norm
+dg_norms_var <- norms1[, dg_columns] %>% summarise_at(vars(answers.1.:answers.9.), var, na.rm=T) %>% 
+  subset.data.frame(select = which.max(dg_appropriateness_sum))
+
+
+
+##############################
 label_count = 1
 
 for (i in 1:8) {
