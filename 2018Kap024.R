2b74fd81c44e8225a6badfae50ad52e79a03c03b
Stefano Pagano <stefano.pagano.90@gmail.com>
2021-07-19 13:00:49 +0200

    Delete 2018Kap024.R

--- 2018Kap024.R
+++
@@ -1,9 +0,0 @@
-## Read in packages
-library(tidyverse)
-library(readxl)
-rm(list = ls())
-
-# set wd 
-setwd("G:/.shortcut-targets-by-id/1IoJDOQWCFiL1qTzSja6byrAlCelNSTsT/Meta-analysis beliefs/Dati paper/2018Kap024/2018Kap024_data")
-
-dg_s1=read.csv("Study1_NHB.csv", sep=",")
