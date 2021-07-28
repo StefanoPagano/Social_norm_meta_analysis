0de7fb059d63555b484a61ae42c125615539a8d0
andrea.guido <andrea.guido.research@gmail.com>
2021-07-28 11:45:01 +0200

    Update - 2017Gac013.R

--- Paper_script/- 2017Gac013.R
+++ Paper_script/- 2017Gac013.R
@@ -6,4 +6,4 @@ rm(list = ls())
 # set wd 
 setwd("G:/.shortcut-targets-by-id/1IoJDOQWCFiL1qTzSja6byrAlCelNSTsT/Meta-analysis beliefs/Dati paper/2017Gac013/2017Gac013_data")
 
-#pgg=read.csv("PG_Data_bysubj.txt", sep="\t")
+dg=read_xls("data_all.xls")
