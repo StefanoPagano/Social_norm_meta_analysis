675ebde5acb3cf31f81da60a659175730ca2e83e
andrea.guido <andrea.guido.research@gmail.com>
2021-10-12 08:41:10 +0200

    Update test_HP.R

--- Analysis/test_HP.R
+++ Analysis/test_HP.R
@@ -1,4 +1,5 @@
 library(ggplot2)
+library()
 
 # read data 
 master <- read.csv("../Paper_csv/Master.csv")
@@ -6,4 +7,4 @@ master <- read.csv("../Paper_csv/Master.csv")
 # H1 : correlation between average cooperation and norm (NE)
 
 cor.test(master$Avg_coop, master$Avg_NE, method= "spearman", exact = F)
-ggplot(data=master, aes(x=))
+ggplot(data=master, aes(x=Avg_NE, y=Avg_coop)) + geom_point() + geom_smooth(method = "lm") + g
