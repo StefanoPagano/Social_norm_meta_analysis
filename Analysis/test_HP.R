13cc0868b023450678d54660a28fb5a7bcfbaaae
andrea.guido <andrea.guido.research@gmail.com>
2021-10-14 12:40:03 +0200

    Update test_HP.R

--- Analysis/test_HP.R
+++ Analysis/test_HP.R
@@ -3,8 +3,23 @@ library()
 
 # read data 
 master <- read.csv("../Paper_csv/Master.csv")
+DG <- master %>% subset.data.frame(Game_type=="DG")
 
 # H1 : correlation between average cooperation and norm (NE)
-
 cor.test(master$Avg_coop, master$Avg_NE, method= "spearman", exact = F)
-ggplot(data=master, aes(x=Avg_NE, y=Avg_coop)) + geom_point() + geom_smooth(method = "lm") + g
+ggplot(data=master, aes(x=Avg_NE, y=Avg_coop)) + geom_point() + geom_smooth(method = "lm")
+
+# H2 : correlation between average cooperation and norm (NE)
+cor.test(master$Avg_coop, master$Var_NE, method= "spearman", exact = F)
+ggplot(data=master, aes(x=Var_NE, y=Avg_coop, color=Game_type)) + geom_point() + geom_smooth()
+
+## DG analysis
+
+# H1
+cor.test(DG$Avg_coop, DG$Avg_NE, method= "spearman", exact = F)
+ggplot(data=DG, aes(x=Avg_NE, y=Avg_coop)) + geom_point() + geom_smooth(method = "lm")
+
+# H2 : correlation between average cooperation and norm (NE)
+cor.test(DG$Avg_coop, DG$Var_NE, method= "spearman", exact = F)
+ggplot(data=DG, aes(x=Var_NE, y=Avg_coop)) + geom_point() + geom_smooth()
+
