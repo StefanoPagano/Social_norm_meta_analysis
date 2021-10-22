534366bb6aca7b0858b81e3fcfc5dc19ff051895
Stefano Pagano <stefano.pagano.90@gmail.com>
2021-10-22 13:13:01 +0200

    c  c

--- Analysis/test_HP.R
+++ Analysis/test_HP.R
@@ -4,7 +4,7 @@ library(tidyverse)
 setwd("C:/Users/stefa/Documents/CNR/GitHub/Social_norm_meta_analysis/")
 
 # read data 
-master <- read.csv("Paper_csv/Master.csv")
+master <- read.csv("Paper_csv/Master.csv") %>% mutate(norm_str = Avg_NE/Var_NE)
 DG <- master %>% subset.data.frame(Game_type=="DG")
 
 # H1 : correlation between average cooperation and norm (NE)
@@ -19,6 +19,10 @@ ggplot(data=master, aes(x=Var_NE, y=Avg_coop, color=Game_type)) + geom_point() +
 cor.test(master$Var_coop, master$Var_NE, method= "spearman", exact = F)
 ggplot(data=master, aes(x=Var_NE, y=Var_coop, color=Game_type)) + geom_point() + geom_smooth()
 
+#H3 
+cor.test(master$Avg_coop, master$norm_str, method= "spearman", exact = F)
+ggplot(data=master, aes(x=norm_str, y=Avg_coop)) + geom_point() + geom_smooth(method = "lm")
+
 ## DG analysis
 
 # H1
@@ -32,3 +36,7 @@ ggplot(data=DG, aes(x=Var_NE, y=Avg_coop)) + geom_point() + geom_smooth()
 # H2b : correlation between variance cooperation and norm (NE)
 cor.test(DG$Var_coop, DG$Var_NE, method= "spearman", exact = F)
 ggplot(data=DG, aes(x=Var_NE, y=Var_coop, color=Game_type)) + geom_point() + geom_smooth()
+
+#H3 
+cor.test(DG$Avg_coop, DG$norm_str, method= "spearman", exact = F)
+ggplot(data=DG, aes(x=norm_str, y=Avg_coop)) + geom_point() + geom_smooth(method = "lm")
