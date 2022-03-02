7890e331ae7916d7e4ef6fc6682bdc4408bf18b0
StefanoPagano <stefano.pagano.90@gmail.com>
2022-03-02 12:17:21 +0100

    paper

---
+++ Paper/Stats.R
@@ -0,0 +1,25 @@
+social_norms <- read_excel("Social Norms meta.xlsx", sheet = "ALL")
+
+# number of paper
+n_paper_total <- nrow(distinct(social_norms %>% select(PaperID)))
+n_treat_total <- nrow(distinct(social_norms %>% select(treatment_id)))
+
+n_paper_first <- nrow(distinct(social_norms %>% filter(Game_type %in% c("DG", "Donation Game", "ToG")) %>% select(PaperID)))
+n_treat_first <- nrow(distinct(social_norms %>% filter(Game_type %in% c("DG", "Donation Game", "ToG")) %>% select(treatment_id)))
+n_treat_ok <- nrow(distinct(social_norms %>% filter(Game_type %in% c("DG", "Donation Game", "ToG") & StatusTreatment_Roma != "1-Not eligible treatment") %>% select(treatment_id)))
+
+print(paste("Total number of papers: ", n_paper_first, "/", n_paper_total, ". Treatments: ", n_treat_first, "/", n_treat_total, sep = ""))
+print(paste("Treatment in target: " , n_treat_ok))
+
+target <- social_norms %>% filter(Game_type %in% c("DG", "Donation Game", "ToG") & StatusTreatment_Roma != "1-Not eligible treatment")
+
+
+target %>% 
+  group_by(Game_type) %>%
+  summarise(n=n(),
+            between=sum(Separate_sample_beliefs=="Y", na.rm = T),
+            KW=sum(Method_elicitation=="KW", na.rm = T),
+            BX=sum(Method_elicitation=="Bicchieri", na.rm = T),
+            KWBX=sum(Method_elicitation=="Both", na.rm = T),
+            OnlyNorm=sum(Choice_Method=="OnlyNorms", na.rm = T),
+            Available=sum(StatusTreatment_Roma=="6-Complete", na.rm = T))
