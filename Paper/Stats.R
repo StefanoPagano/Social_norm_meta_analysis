b036401bb88675f45cb8d5972aeffecb37ccc1e3
Stefano Pagano <stefano.pagano.90@gmail.com>
2022-03-04 18:48:00 +0100

    Update Stats.R

--- Paper/Stats.R
+++ Paper/Stats.R
@@ -1,4 +1,4 @@
-social_norms <- read_excel("Social Norms meta.xlsx", sheet = "ALL")
+social_norms <- read_xlsx(path = "G:/.shortcut-targets-by-id/1IoJDOQWCFiL1qTzSja6byrAlCelNSTsT/Meta-analysis beliefs/Social Norms meta.xlsx", sheet = "ALL")
 
 # number of paper
 n_paper_total <- nrow(distinct(social_norms %>% select(PaperID)))
@@ -6,12 +6,12 @@ n_treat_total <- nrow(distinct(social_norms %>% select(treatment_id)))
 
 n_paper_first <- nrow(distinct(social_norms %>% filter(Game_type %in% c("DG", "Donation Game", "ToG")) %>% select(PaperID)))
 n_treat_first <- nrow(distinct(social_norms %>% filter(Game_type %in% c("DG", "Donation Game", "ToG")) %>% select(treatment_id)))
-n_treat_ok <- nrow(distinct(social_norms %>% filter(Game_type %in% c("DG", "Donation Game", "ToG") & StatusTreatment_Roma != "1-Not eligible treatment") %>% select(treatment_id)))
+n_treat_ok <- nrow(distinct(social_norms %>% filter(Game_type %in% c("DG", "Donation Game", "ToG") & StatusTreatment_Roma != "1-Not eligible treatment" & Method_elicitation!="Bicchieri" & Baseline %in% c(1, NA) & Standard_game != "N") %>% select(treatment_id)))
 
 print(paste("Total number of papers: ", n_paper_first, "/", n_paper_total, ". Treatments: ", n_treat_first, "/", n_treat_total, sep = ""))
 print(paste("Treatment in target: " , n_treat_ok))
 
-target <- social_norms %>% filter(Game_type %in% c("DG", "Donation Game", "ToG") & StatusTreatment_Roma != "1-Not eligible treatment")
+target <- social_norms %>% filter(Game_type %in% c("DG", "Donation Game", "ToG") & StatusTreatment_Roma != "1-Not eligible treatment" & Method_elicitation!="Bicchieri" & Baseline %in% c(1, NA) & Standard_game != "N")
 
 
 target %>% 
