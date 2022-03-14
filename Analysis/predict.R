419644984f5f8c5777c958363c32792cc3355875
Stefano Pagano <stefano.pagano.90@gmail.com>
2022-03-14 12:13:44 +0100

    Create predict.R

---
+++ Analysis/predict.R
@@ -0,0 +1,21 @@
+treatment="2019Cha026_3"
+
+df_master <- utility_df_treatment_aic_analysis %>% 
+  filter(treatment_id==treatment)
+
+social_expectation=clogit(A ~ payoff + mean_app + strata(subject_id), 
+                                data=df_master)
+
+inequality_aversion=clogit(A ~ other_payoff_ahead + other_payoff_behind + sqr_other_payoff_ahead + sqr_other_payoff_behind+ strata(subject_id), 
+                                 data=df_master)
+
+inequality_aversion2=clogit(A ~ payoff + other_payoff_ahead + other_payoff_behind  + strata(subject_id), 
+                           data=df_master)
+
+social_expectation_aug1=clogit(A ~ payoff + mean_app + log_sd_app + strata(subject_id), 
+                                data=df_master)
+
+
+tab_model(social_expectation, inequality_aversion, social_expectation_aug1, dv.labels = c("Social Exp", "Ineq. Avers.", "Soc. Exp. Aug."), show.aic = T, show.loglik = T, show.se = T, show.ci = F, title = treatment, transform=NULL)
+
+barplot(predict(inequality_aversion, type = "expected")[1:11])
